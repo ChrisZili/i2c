1cd99338af976a2b3ad4950a92e4a89ced79f6fa
christianzilincar <zilincarchristian@gmail.com>
2023-11-06 19:57:56 +0100

    i2c communication

--- Core/Src/i2c.c
+++ Core/Src/i2c.c
@@ -1,4 +1,3 @@
-/* USER CODE BEGIN Header */
 /**
   ******************************************************************************
   * @file    i2c.c
@@ -7,21 +6,24 @@
   ******************************************************************************
   * @attention
   *
-  * Copyright (c) 2023 STMicroelectronics.
-  * All rights reserved.
+  * <h2><center>&copy; Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.</center></h2>
   *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
   *
   ******************************************************************************
   */
-/* USER CODE END Header */
+
 /* Includes ------------------------------------------------------------------*/
 #include "i2c.h"
 
 /* USER CODE BEGIN 0 */
-uint8_t i2c_rx_data = 0;
+uint8_t *aReceiveBuffer_read , end_of_read_flag = 0;
+volatile uint8_t ubReceiveIndex = 0;
+
 /* USER CODE END 0 */
 
 /* I2C1 init function */
@@ -45,7 +47,7 @@ void MX_I2C1_Init(void)
   GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
   GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_HIGH;
   GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_OPENDRAIN;
-  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_UP;
   GPIO_InitStruct.Alternate = LL_GPIO_AF_4;
   LL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
@@ -59,7 +61,6 @@ void MX_I2C1_Init(void)
   /* USER CODE BEGIN I2C1_Init 1 */
 
   /* USER CODE END I2C1_Init 1 */
-
   /** I2C Initialization
   */
   LL_I2C_EnableAutoEndMode(I2C1);
@@ -76,53 +77,67 @@ void MX_I2C1_Init(void)
   LL_I2C_Init(I2C1, &I2C_InitStruct);
   LL_I2C_SetOwnAddress2(I2C1, 0, LL_I2C_OWNADDRESS2_NOMASK);
   /* USER CODE BEGIN I2C1_Init 2 */
-
+  LL_I2C_Enable(I2C1);
   /* USER CODE END I2C1_Init 2 */
 
 }
 
 /* USER CODE BEGIN 1 */
-uint8_t i2c_master_read_byte(uint8_t slave_address, uint8_t register_address)
-{
-	// Enable It from I2C
-	LL_I2C_EnableIT_RX(I2C1);
-	// Initialize communication
-	LL_I2C_HandleTransfer(I2C1, slave_address, LL_I2C_ADDRSLAVE_7BIT, 1, LL_I2C_MODE_AUTOEND, LL_I2C_GENERATE_START_WRITE);
-	// Send register address
-	while(!LL_I2C_IsActiveFlag_STOP(I2C1))
-	{
-		if(LL_I2C_IsActiveFlag_TXIS(I2C1))
-		{
-			LL_I2C_TransmitData8(I2C1, register_address);
+void master_write(uint8_t data, uint8_t register_addr, uint8_t slave_addr,
+		uint8_t read_flag) {
+	if (read_flag) {
+		register_addr |= (1 << 7);
+	}
+	LL_I2C_HandleTransfer(I2C1, slave_addr, LL_I2C_ADDRSLAVE_7BIT, 2,
+			LL_I2C_MODE_AUTOEND, LL_I2C_GENERATE_START_WRITE);
+	LL_I2C_TransmitData8(I2C1, register_addr);
+	while (!LL_I2C_IsActiveFlag_STOP(I2C1)) {
+		if (LL_I2C_IsActiveFlag_TXIS(I2C1)) {
+			LL_I2C_TransmitData8(I2C1, data);
 		}
 	}
 	LL_I2C_ClearFlag_STOP(I2C1);
-	while(LL_I2C_IsActiveFlag_STOP(I2C1)){};
+}
+uint8_t* master_read(uint8_t *buffer, uint8_t length, uint8_t register_addr,
+		uint8_t slave_addr, uint8_t read_flag) {
+	aReceiveBuffer_read = buffer;
+	if (read_flag) {
+		register_addr |= (1 << 7);
+	}
+	end_of_read_flag = 0;
+	LL_I2C_EnableIT_RX(I2C1);
 
-	// Receive data from slave device
-	LL_I2C_HandleTransfer(I2C1, slave_address, LL_I2C_ADDRSLAVE_7BIT, 1, LL_I2C_MODE_AUTOEND, LL_I2C_GENERATE_START_READ);
-	while(!LL_I2C_IsActiveFlag_STOP(I2C1)){};
+	//
+	LL_I2C_HandleTransfer(I2C1, slave_addr, LL_I2C_ADDRSLAVE_7BIT, 1,
+			LL_I2C_MODE_AUTOEND, LL_I2C_GENERATE_START_WRITE);
+	while (!LL_I2C_IsActiveFlag_STOP(I2C1)) {
+		if (LL_I2C_IsActiveFlag_TXIS(I2C1)) {
+			LL_I2C_TransmitData8(I2C1, register_addr);
+		}
+	}
+	LL_I2C_ClearFlag_STOP(I2C1);
+	while (LL_I2C_IsActiveFlag_STOP(I2C1)) {
+	}
+	//citam register od slejva
+	LL_I2C_HandleTransfer(I2C1, slave_addr, LL_I2C_ADDRSLAVE_7BIT, length,
+			LL_I2C_MODE_AUTOEND, LL_I2C_GENERATE_START_READ);
+	while (!LL_I2C_IsActiveFlag_STOP(I2C1)) {
+	};
 
 	//End of transfer
-	LL_I2C_DisableIT_RX(I2C1);
 	LL_I2C_ClearFlag_STOP(I2C1);
-	LL_I2C_ClearFlag_NACK(I2C1);
+	LL_I2C_DisableIT_RX(I2C1);
+	I2C1->ICR |= (1 << 4);
+	ubReceiveIndex = 0;
+	end_of_read_flag = 1;
 
-	return i2c_rx_data;
+	return aReceiveBuffer_read;
 }
-void I2C1_EV_IRQHandler(void)
-{
-  /* USER CODE BEGIN I2C1_EV_IRQn 0 */
-
-  /* USER CODE END I2C1_EV_IRQn 0 */
-	if(LL_I2C_IsActiveFlag_RXNE(I2C1))
-		{
-			// Call function Master Reception Callback
-			i2c_rx_data = LL_I2C_ReceiveData8(I2C1);
-		}
-  /* USER CODE BEGIN I2C1_EV_IRQn 1 */
-
-  /* USER CODE END I2C1_EV_IRQn 1 */
+void Master_Reception_Callback(void) {
+	aReceiveBuffer_read[ubReceiveIndex++] = LL_I2C_ReceiveData8(I2C1);
+			(ubReceiveIndex > 19) ? ubReceiveIndex = 0 : ubReceiveIndex;
+			end_of_read_flag = 0;
 }
-
 /* USER CODE END 1 */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
